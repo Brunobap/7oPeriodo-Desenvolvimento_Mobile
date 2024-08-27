d349c1cf37b2973ffc19cddcd9eee1c98f36a7ba
Bruno Ribeiro Batista <bruno@engsupport.eti.br>
2024-08-26 21:04:16 -0300

    26/08/2024

---
+++ Trabalho - Pop Quiz/pop_quiz/app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Menu Principal',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="novaPartida"
+        options={{
+          title: 'Nova Partida',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'game-controller' : 'game-controller-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="meuPerfil"
+        options={{
+          title: 'Meu perfil',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'man' : 'man-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ Trabalho - Pop Quiz/pop_quiz/app/teste.tsx
@@ -0,0 +1,31 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TesteScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Bolas!' }} />
+      <ThemedView style={styles.container}>
+        <Link href="/" style={styles.link}>
+          <ThemedText type="link">Deu certo!</ThemedText>
+        </Link>
+      </ThemedView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
